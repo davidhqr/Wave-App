749d89d4a456cb1c04f4405622b6592bd9b0f097
David Zhang <david.zhang2@shopify.com>
2019-03-13 16:15:42 -0400

    Refactor SendImagePage

--- lib/pages/send_image_page.dart
+++ lib/pages/send_image_page.dart
@@ -15,9 +15,9 @@ class SendImagePage extends StatefulWidget {
 }
 
 class _SendImagePageState extends State<SendImagePage> {
-  String _imagePath;
-
   final Logger log = new Logger('WaveRequest');
+
+  String _imagePath;
 
   void _pickImage() async {
     try {
@@ -46,64 +46,69 @@ class _SendImagePageState extends State<SendImagePage> {
     request.send();
   }
 
+  Widget _imageDisplay() {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 280,
+            width: 280,
+            decoration: new BoxDecoration(
+                color: Colors.white70,
+                borderRadius: BorderRadius.all(const Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFbfbfbf),
+                    blurRadius: 20,
+                    // has the effect of softening the shadow
+                  )
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: _imagePath == null
+                  ? Center(child: Text('No image selected'))
+                  : Image.file(new File(_imagePath)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _sendImageButton(BuildContext context) {
+    return RaisedButton(
+      color: Color(0xFFfa7268),
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      onPressed: () {
+        _sendImage(context);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text(
+          'Send Image Wave',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Builder(
-        builder: (BuildContext context) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 280,
-                      width: 280,
-                      decoration: new BoxDecoration(
-                          color: Colors.white70,
-                          borderRadius: BorderRadius.all(const Radius.circular(20)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color(0xFFbfbfbf),
-                              blurRadius: 20,
-                              // has the effect of softening the shadow
-                            )
-                          ]),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: _imagePath == null
-                            ? Center(child: Text('No image selected'))
-                            : Image.file(new File(_imagePath)),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              RaisedButton(
-                color: Color(0xFFfa7268),
-                elevation: 2,
-                shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                onPressed: () {
-                  _sendImage(context);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Text(
-                    'Send Image Wave',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-              ),
-            ],
-          );
-        }
-      ),
+      body: Builder(builder: (BuildContext context) {
+        return Column(
+          children: [
+            _imageDisplay(),
+            _sendImageButton(context),
+          ],
+        );
+      }),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xFFfa7268),
         onPressed: _pickImage,
