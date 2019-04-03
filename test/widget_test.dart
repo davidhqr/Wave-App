ba51fcb61a03cc80722cdd0fe0b89865f22ebcdf
David Zhang <david.zhang2@shopify.com>
2019-04-03 19:44:12 -0400

    Fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Wave/main.dart';
+import 'package:wave/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
